6dbecc73df717544073513fe2690223d910678f2
Milind <milind.official98@gmail.com>
2024-07-20 15:34:24 +0200

    Switch from PostgresSQL to Sqlite3 for database

--- MyWork/app/config.py
+++ MyWork/app/config.py
@@ -1,9 +1,10 @@
-# Configuration Settings
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:123@localhost/health_monitor')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True,
@@ -11,3 +12,21 @@ class Config:
         'pool_size': 10,
         'max_overflow': 20
     }
+
+
+
+# Following is for PostgresSql
+
+# # Configuration Settings
+# import os
+
+# class Config:
+#     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:123@localhost/health_monitor')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SQLALCHEMY_ENGINE_OPTIONS = {
+#         'pool_pre_ping': True,
+#         'pool_recycle': 300,
+#         'pool_size': 10,
+#         'max_overflow': 20
+#     }
